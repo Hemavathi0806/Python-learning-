84c7912a20bb33a54201aaac0ba5d0b302019904
B Hemavathi <ganidhruva05@gmail.com>
2026-05-13 09:54:01 -0700

    Data types.py
    Data types and examples

---
+++ data types.py
@@ -0,0 +1,46 @@
+# data types
+#1.what is data types
+#data types means type of value stored in a variable
+#2 .Which data type is used to store whole numbers?
+# to store whole numbers we use int()
+#3.Which data type is used to store decimal values?
+# to store decimal values we use
+#float()
+#4.Which data type is used to store text? A:string()
+#5.Which data type stores True or False values? A: boolean() or bool()
+#6.Create one integer variable and print it
+a=10
+print(a)
+print(type(a))
+#7.Create one float variable and print it
+b=34.4
+print(b)
+print(type(b))
+#8.Create one string variable and print it
+name="Hema"
+print(type(name))
+#9.Create one bool variable and print it
+is_student=True
+print(is_student)
+#10.Identify the data type
+a = 25
+#11.Identify the data type
+b = 25.7
+#12.What is the output?
+print(type(10))
+#13.What is the output?
+print(type("Python"))
+#14.What is the output?
+print(type(False))
+#15.Can variable names start with numbers?  A: No 
+#16.Which function is used to check data type? A: type()
+#17.Create variables for:
+# student name
+#age
+#passed status
+#marks
+student_name="Hema"
+age=19
+passed=True
+marks=100
+
