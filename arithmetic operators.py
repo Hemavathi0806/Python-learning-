153c4127e5a82dede959193bf60f415d2805b68c
B Hemavathi <ganidhruva05@gmail.com>
2026-05-13 11:10:39 -0700

    Operators
    Arithmetic operations using operators

---
+++ arithmetic operators.py
@@ -0,0 +1,32 @@
+#Topic: Arithmetic Operators
+#1.What are arithmetic operators?
+#arithmetic operators are used to perfom arithmetic operations like addition,substraction,multiplication,division.
+#2.Which operator is used for addition?  Ans: +(plus)
+#3.Which operator is used for subtraction? Ans: -
+#4.Which operator is used for multiplication? Ans:  *
+#5.which operator is used for division ?  Ans:/
+#6.which operator gives remainder value? Ans: %
+#7.which operator is used for power? Ans: ^
+#14.Create a small calculator program
+#a = 10
+#b = 5
+#addition#subtraction#multiplication#division
+a = 10
+b = 5
+# addition
+add = a +b
+print("Addition:", add)
+# subtraction
+sub = a - b
+print("Subtraction:", sub)
+# multiplication
+mul = a * b
+print("Multiplication:", mul)
+# division
+div = a / b
+print("Division:", div)
+
+
+
+
+
